b16ebae8e91d8f5bfe8eed5bc09453d8545804aa
Viacheslav_Belymenko <Viacheslav_Belymenko@epam.com>
2018-12-13 14:06:04 +0200

    Handle login/logout action for user.

--- src/components/header-app/HeaderApp.jsx
+++ src/components/header-app/HeaderApp.jsx
@@ -3,9 +3,28 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 import './HeaderApp.css';
 
-export class HeaderApp extends Component {
+export class HeaderAppContainer extends Component {
+
+
+    state = {
+        isLoggedIn: false
+    };
+
+    componentDidMount() {
+        this.setState({
+            isLoggedIn: localStorage.getItem('user') ? true : false
+        });
+    }
+
+    handleLogoutClick = () => {
+        localStorage.removeItem('user');
+        this.props.history.push(`/courses`);
+    }
+
     render() {
         return (
             <div className='header-root'>
@@ -14,10 +33,14 @@ export class HeaderApp extends Component {
                         <Typography variant="display2" color="inherit" className='grow'>
                             Courses
           </Typography>
-                        <Button className='login-button' color="inherit">Login</Button>
+                        {this.state.isLoggedIn ?
+                            <Button className='login-button' onClick={this.handleLogoutClick} color="inherit">Logout</Button> :
+                            <Link to={'/login'}><Button className='login-button' color="inherit">Login</Button></Link>}
                     </Toolbar>
                 </AppBar>
             </div>
         );
     }
 }
+
+export const HeaderApp = withRouter(HeaderAppContainer);
